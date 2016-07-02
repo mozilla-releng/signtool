e1b8a495e773a7c5f32d204c8219f8096c72eb52
Aki Sasaki <aki@escapewindow.com>
2016-07-01 21:11:43 -0700

    nuke some unused functions

--- signtool/util/paths.py
+++ signtool/util/paths.py
@@ -9,31 +9,6 @@ from subprocess import PIPE, Popen
 log = logging.getLogger(__name__)
 
 
-def windows2msys(path):
-    """Translate a Windows pathname to an MSYS pathname.
-    Necessary because we call out to ssh/scp, which are MSYS binaries
-    and expect MSYS paths."""
-    if not sys.platform.startswith('win'):
-        return path
-    (drive, path) = os.path.splitdrive(os.path.abspath(path))
-    return "/" + drive[0] + path.replace('\\', '/')
-
-
-def msys2windows(path):
-    """ Translate an MSYS pathname to Windows.
-        Possibly needed for pymake. """
-    if not sys.platform.startswith('win'):
-        return path
-    if '/' not in path:
-        return path.replace('\\', '\\\\\\\\')
-    strippedpath = path.lstrip('/').rstrip('/')
-    subdirs = strippedpath.split('/')
-    # Check if first dir of msyspath is the drive letter
-    if len(subdirs[0]) != 1:
-        return path.replace('\\', '\\\\\\\\')
-    return subdirs[0].upper() + ':\\' + '\\'.join(subdirs[1:]).replace('\\', '\\\\\\\\')
-
-
 def cygpath(filename):
     """Convert a cygwin path into a windows style path"""
     if sys.platform == 'cygwin':
@@ -73,12 +48,6 @@ def findfiles(roots, includes=['*'], excludes=[]):
         else:
             retval.append(fn)
     return retval
-
-
-def relpath(d1, d2):
-    """Returns d1 relative to d2"""
-    assert d1.startswith(d2)
-    return d1[len(d2):].lstrip('/')
 
 
 def finddirs(root):
